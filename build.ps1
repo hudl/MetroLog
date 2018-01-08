9e9b2129ec61e8afd1e15bb84f25f80065cc698c
Jared Barboza <jared.m.barboza@gmail.com>
2018-01-08 14:05:34 -0600

    NuGet restore fixed, build checks for failure at each step

--- build.ps1
+++ build.ps1
@@ -4,6 +4,8 @@ param (
    [string]$nugetPushSource
 )
 
+$ErrorActionPreference = "Stop"
+
 $target = "Release"
 $branch = git rev-parse --abbrev-ref HEAD
 $branch = $branch -replace '[-]',''
@@ -23,16 +25,31 @@ Write-Host "TARGET: $target"
 Write-Host "VERSION: $version"
 Write-Host "MSBUILD: $msbuildExe"
 
-./.nuget/nuget.exe restore
+./.nuget/nuget.exe restore .\HudlMetroLog.sln
+
+if ($LastExitCode -ne 0) {
+  throw "NuGet restore failed with exit code $LastExitCode"
+}
 
 ./set-version.ps1 $version
 
 &$msbuildExe HudlMetroLog.sln /m /p:platform=Hudl /p:Configuration=Release /nologo
 
+if ($LastExitCode -ne 0) {
+  throw "MSBuild failed with exit code $LastExitCode"
+}
+
 ./.nuget/nuget.exe pack HudlMetroLog.nuspec -version $version -prop "target=$target" -NoPackageAnalysis
+
+if ($LastExitCode -ne 0) {
+  throw "NuGet pack failed with exit code $LastExitCode"
+}
 
 if ([string]::IsNullOrEmpty($nugetApiKey) -ne $true) {
   ./.nuget/nuget.exe push "HudlMetroLog.$version.nupkg" -ApiKey $nugetApiKey -Source $nugetPushSource
+  if ($LastExitCode -ne 0) {
+    throw "NuGet push failed with exit code $LastExitCode"
+  }
 } else {
   write-host "NuGet API key not supplied. will not publish."
 }
